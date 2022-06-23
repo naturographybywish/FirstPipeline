22067db7131c736a00ac8a0ef62b68f79d6cd322
naturographybywish <naturographybywish@gmail.com>
2022-06-23 12:44:00 +0530

    first commit

---
+++ src/test/java/com/inv/main/FirstPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
